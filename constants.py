0d52e9347463af909118e478b330987d511ed81e
godot63 <lcalmbach@gmail.com>
2020-12-29 09:09:46 +0100

    added os.path.join

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+import os
+
 APP_INFO = """A GUI for the pandemic simulator proposed by by [Terence S](https://terenceshin.medium.com/) in [Simulating the Pandemic in Python](https://towardsdatascience.com/simulating-the-pandemic-in-python-2aa8f7383b55)
     Change settings under the `Define scenario` menu item. Then select `Run scenario` and press `Start Simulation`.
     """
@@ -5,7 +7,8 @@ GIT_INFO = """SPS version {}. This app was implemented by [Lukas Calmbach](mailt
 The simulation code was adapted from [Terence Shin](https://terenceshin.medium.com/).
 The code can be found on [github](https://github.com/lcalmbach/pandemic-simulator)"""
 MENU_LIST = ['Info', 'Define scenario', 'Run Scenario']
-SETTINGS_FILENAME = './tmp/scenarios.json'
-POPULATION_FILENAME = './tmp/population.csv'
-INFECTIONS_FILENAME = './tmp/infections.csv'
-TIMESERIES_FILENAME = './tmp/timeseries.csv'
+
+SETTINGS_FILENAME = os.path.join("tmp", "scenarios.json")
+POPULATION_FILENAME = os.path.join("tmp", "population.json")
+INFECTIONS_FILENAME = os.path.join("tmp", "infections.json")
+TIMESERIES_FILENAME = os.path.join("tmp", "timeseries.json")
